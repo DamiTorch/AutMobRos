4d4706534aac098b1e2b8e748f789d3f06b28e71
Damian Kuratli <damian.kuratli@bluewin.ch>
2025-04-29 10:27:13 +0000

    Week09 - Excercise: Safety System

--- inc/MyRobotSafetyProperties.hpp
+++ inc/MyRobotSafetyProperties.hpp
@@ -11,19 +11,39 @@ public:
     MyRobotSafetyProperties(ControlSystem &cs, double dt);
 
     // Define all possible events
-    eeros::safety::SafetyEvent doSystemOff;
-    eeros::safety::SafetyEvent doSystemOn;
+    // upwards events
+    eeros::safety::SafetyEvent doSystemOn;          // blue
+    eeros::safety::SafetyEvent systemStarted;       // blue
+    eeros::safety::SafetyEvent resetEmergency;      // light green
+    eeros::safety::SafetyEvent powerOn;             // violet
+    eeros::safety::SafetyEvent startMoving;         // brown
+    // downwards events
+    eeros::safety::SafetyEvent shutdown;            // purple
+    eeros::safety::SafetyEvent motorsHalted;        // yellow
+    eeros::safety::SafetyEvent abort;               // red
+    eeros::safety::SafetyEvent emergency;           // brown
+    eeros::safety::SafetyEvent powerOff;            // light blue
+    eeros::safety::SafetyEvent stopMoving;          // green
 
     // Defina all possible levels
-    eeros::safety::SafetyLevel slSystemOff;
-    eeros::safety::SafetyLevel slSystemOn;
+    eeros::safety::SafetyLevel slSystemOff;         // 0
+    eeros::safety::SafetyLevel slShuttingDown;      // 1
+    eeros::safety::SafetyLevel slBraking;           // 2
+    eeros::safety::SafetyLevel slStartingUp;        // 3
+    eeros::safety::SafetyLevel slEmergency;         // 4
+    eeros::safety::SafetyLevel slEmergencyBraking;  // 5
+    eeros::safety::SafetyLevel slSystemOn;          // 6
+    eeros::safety::SafetyLevel slMotorPowerOn;      // 7
+    eeros::safety::SafetyLevel slSystemMoving;      // 8
 
 private:
     // Define all critical outputs
-    // eeros::hal::Output<bool>* ...;
+    eeros::hal::Output<bool>* greenLED;
+    eeros::hal::Output<bool>* redLED;
 
     // Define all critical inputs
-    // eeros::hal::Input<bool>* ...;
+    eeros::hal::Input<bool>* buttonPause;
+    eeros::hal::Input<bool>* buttonMode;
 
     ControlSystem &cs;
 };
--- src/MyRobotSafetyProperties.cpp
+++ src/MyRobotSafetyProperties.cpp
@@ -4,48 +4,146 @@ MyRobotSafetyProperties::MyRobotSafetyProperties(ControlSystem &cs, double dt)
     : cs(cs),
     
       slSystemOff("System is offline"),
+      slShuttingDown("System is shutting down (transition)"),
+      slBraking("System is braking (transition)"),
+      slStartingUp("System is in the starting up phase"),
+      slEmergency("Emergency"),
+      slEmergencyBraking("System does emergency brake"),
       slSystemOn("System is online"),
+      slMotorPowerOn("System with Motor Power on"),
+      slSystemMoving("System is moving"),
 
       doSystemOn("Startup the system"),
-      doSystemOff("Shutdown the system")
+      systemStarted("Set the system to IDLE"),
+      resetEmergency("Going back to IDLE after a Emergency"),
+      powerOn("Enable Motor Power"),
+      startMoving("System is Moving"),
+      shutdown("Shutdown the system"),
+      motorsHalted("Transition till system Stopps"),
+      abort("Any type of aborts"),
+      emergency("Any type of Emergency"),
+      powerOff("Switch Motor Power off"),
+      stopMoving("System is Stopping")
+      
 {
     eeros::hal::HAL &hal = eeros::hal::HAL::instance();
 
     // Declare and add critical outputs
-    // ... = hal.getLogicOutput("...");
+    greenLED = hal.getLogicOutput("onBoardLEDgreen");
+    redLED = hal.getLogicOutput("onBoardLEDred");
 
-    // criticalOutputs = { ... };
+    criticalOutputs = { greenLED, redLED };
 
     // Declare and add critical inputs
-    // ... = eeros::hal::HAL::instance().getLogicInput("...", ...);
+    buttonPause = eeros::hal::HAL::instance().getLogicInput("onBoardButtonPause");
+    buttonMode = eeros::hal::HAL::instance().getLogicInput("onBoardButtonMode");
 
-    // criticalInputs = { ... };
+    criticalInputs = { buttonPause, buttonMode };
 
     // Add all safety levels to the safety system
     addLevel(slSystemOff);
+    addLevel(slShuttingDown);
+    addLevel(slBraking);
+    addLevel(slStartingUp);
+    addLevel(slEmergency);
+    addLevel(slEmergencyBraking);
     addLevel(slSystemOn);
+    addLevel(slMotorPowerOn);
+    addLevel(slSystemMoving);
 
     // Add events to individual safety levels
-    slSystemOff.addEvent(doSystemOn, slSystemOn, kPublicEvent);
-    slSystemOn.addEvent(doSystemOff, slSystemOff, kPublicEvent);
+    // TODO: Question: What are private and public events ??
+    slSystemOff.addEvent(doSystemOn, slStartingUp, kPublicEvent);
+    slStartingUp.addEvent(systemStarted, slSystemOn, kPrivateEvent);
+    slEmergency.addEvent(resetEmergency, slSystemOn, kPrivateEvent);
+    slSystemOn.addEvent(powerOn, slMotorPowerOn, kPublicEvent);
+    slMotorPowerOn.addEvent(startMoving, slSystemMoving, kPublicEvent);
+    slSystemMoving.addEvent(emergency, slEmergencyBraking, kPublicEvent);
+    slSystemMoving.addEvent(stopMoving, slMotorPowerOn, kPublicEvent);
+    slMotorPowerOn.addEvent(powerOff, slSystemOn, kPublicEvent);
+    slEmergencyBraking.addEvent(motorsHalted, slEmergency, kPrivateEvent);
+    slSystemMoving.addEvent(abort, slBraking, kPublicEvent);
+    slBraking.addEvent(motorsHalted, slShuttingDown, kPrivateEvent);
+    slShuttingDown.addEvent(shutdown, slSystemOff, kPrivateEvent);
+
 
     // Add events to multiple safety levels
-    // addEventToAllLevelsBetween(lowerLevel, upperLevel, event, targetLevel, kPublicEvent/kPrivateEvent);
+    addEventToAllLevelsBetween(slEmergency, slMotorPowerOn, abort, slShuttingDown, kPublicEvent);
+    addEventToAllLevelsBetween(slSystemOn, slMotorPowerOn, emergency, slEmergency, kPublicEvent);
+
 
     // Define input actions for all levels
-    // level.setInputActions({ ... });
+    slSystemOff.setInputActions({           ignore(buttonPause),                    ignore(buttonMode) });
+    slShuttingDown.setInputActions({        ignore(buttonPause),                    ignore(buttonMode) });
+    slBraking.setInputActions({             ignore(buttonPause),                    ignore(buttonMode) });
+    slStartingUp.setInputActions({          ignore(buttonPause),                    ignore(buttonMode) });
+    slEmergency.setInputActions({           ignore(buttonPause),                    check(buttonMode, false, resetEmergency) });
+    slEmergencyBraking.setInputActions({    ignore(buttonPause),                    ignore(buttonMode) });
+    slSystemOn.setInputActions({            check(buttonPause, false, emergency),   ignore(buttonMode) });
+    slMotorPowerOn.setInputActions({        check(buttonPause, false, emergency),   ignore(buttonMode) });
+    slSystemMoving.setInputActions({        check(buttonPause, false, emergency),   ignore(buttonMode) });
+
 
     // Define output actions for all levels
-    // level.setOutputActions({ ... });
+    slSystemOff.setOutputActions({           set(greenLED, false),   set(redLED, false) });
+    slShuttingDown.setOutputActions({        set(greenLED, false),   set(redLED, true) });
+    slBraking.setOutputActions({             set(greenLED, false),   set(redLED, true) });
+    slStartingUp.setOutputActions({          set(greenLED, true),    set(redLED, false) });
+    slEmergency.setOutputActions({           set(greenLED, true),    set(redLED, true) });
+    slEmergencyBraking.setOutputActions({    set(greenLED, true),    set(redLED, true) });
+    slSystemOn.setOutputActions({            set(greenLED, true),    set(redLED, false) });
+    slMotorPowerOn.setOutputActions({        set(greenLED, true),    set(redLED, false) });
+    slSystemMoving.setOutputActions({        set(greenLED, true),    set(redLED, false) });
+
 
     // Define and add level actions
     slSystemOff.setLevelAction([&](SafetyContext *privateContext) {
-        cs.timedomain.stop();
         eeros::Executor::stop();
     });
 
-    slSystemOn.setLevelAction([&](SafetyContext *privateContext) {
+    slShuttingDown.setLevelAction([&](SafetyContext *privateContext) {
+        cs.timedomain.stop();
+        privateContext->triggerEvent(shutdown);
+    });
+
+    slBraking.setLevelAction([&](SafetyContext *privateContext) {
+        // Check if motors are standing sill
+        privateContext->triggerEvent(motorsHalted);
+    });
+
+    slStartingUp.setLevelAction([&](SafetyContext *privateContext) {
         cs.timedomain.start();
+        privateContext->triggerEvent(systemStarted);
+    });
+
+    slEmergency.setLevelAction([&](SafetyContext *privateContext) {
+        
+    });
+
+    slEmergencyBraking.setLevelAction([&](SafetyContext *privateContext) {
+        // Check if motors are standing still
+        privateContext->triggerEvent(motorsHalted);
+    });
+
+    slSystemOn.setLevelAction([&, dt](SafetyContext *privateContext) {
+        if (slSystemOn.getNofActivations()*dt >= 1)   // wait 1 sec
+        {
+            privateContext->triggerEvent(powerOn);
+        }
+    });
+
+    slMotorPowerOn.setLevelAction([&, dt](SafetyContext *privateContext) {
+        if (slMotorPowerOn.getNofActivations()*dt >= 5)   // wait 5 sec
+        {
+            privateContext->triggerEvent(startMoving);
+        }
+    });
+
+    slSystemMoving.setLevelAction([&, dt](SafetyContext *privateContext) {
+        if (slSystemMoving.getNofActivations()*dt >= 5)   // wait 5 sec
+        {
+            privateContext->triggerEvent(stopMoving);
+        }
     });
 
     // Define entry level
@@ -53,6 +151,6 @@ MyRobotSafetyProperties::MyRobotSafetyProperties(ControlSystem &cs, double dt)
 
     // Define exit function
     exitFunction = ([&](SafetyContext *privateContext) {
-        privateContext->triggerEvent(doSystemOff);
+        privateContext->triggerEvent(abort);
     });
 }
