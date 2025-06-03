356bc9cd4e6cc2f786509c0d6377050718929795
Damian Kuratli <damian.kuratli@bluewin.ch>
2025-06-03 10:14:19 +0000

    Week13 - Excercise1: PD Position Control

--- inc/ControlSystem.hpp
+++ inc/ControlSystem.hpp
@@ -4,6 +4,8 @@
 #include <eeros/control/TimeDomain.hpp>
 #include <eeros/core/Executor.hpp>
 #include <eeros/control/PeripheralInput.hpp>
+#include <eeros/control/Sum.hpp>
+#include <eeros/control/D.hpp>
 #include <eeros/control/Gain.hpp>
 #include <eeros/control/Saturation.hpp>
 #include <eeros/control/PeripheralOutput.hpp>
@@ -16,8 +18,14 @@ public:
     ControlSystem(double dt);
 
     // Define Blocks
+    PeripheralInput<> E1;
     PeripheralInput<> E2;
-    Gain<> cont;
+    Sum<> e;
+    Gain<> kp;
+    D<> ed;
+    Gain<> kd;
+    Sum<> qdd_c;
+    Gain<> M;
     Saturation<> QMax;
     Gain<> iInv;
     Gain<> kMInv;
